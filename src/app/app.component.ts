4161f7c9a7c003188128496922ed648b4fd22a67
hopelesscoder <dadda-1993@hotmail.it>
2018-08-29 20:09:43 +0200

    added static download button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileSaver } from 'file-saver/FileSaver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  
+  downloadPdf(){
+    var data = new Blob(['testo di prova'], { type: 'text/plain;charset=utf-8' });
+    return FileSaver.saveAs(data, 'text.txt');
+  }
+  
 }
